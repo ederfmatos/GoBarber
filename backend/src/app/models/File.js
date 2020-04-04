f6eefba14c5324f578b2f89debdf9bd574ee59ed
ederfmatos <ederfmatos@gmail.com>
2020-04-03 21:49:48 -0300

    Avatar do usuario

---
+++ backend/src/app/models/File.js
@@ -0,0 +1,19 @@
+import { Model, STRING } from 'sequelize';
+
+class File extends Model {
+	static init(sequelize) {
+		super.init(
+			{
+				name: STRING,
+				path: STRING,
+			},
+			{
+				sequelize,
+			},
+		);
+
+		return this;
+	}
+}
+
+export default File;
