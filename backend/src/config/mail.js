22992864f93ba89283adb8d58e1df8050fb88efd
ederfmatos <ederfmatos@gmail.com>
2020-04-04 11:09:23 -0300

    Configurando Nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: '8ce79ba5a534cf',
+		pass: 'fabd6f6cb865a2',
+	},
+	default: {
+		from: 'Equipe GoBarber <noreply@gobarber.com>',
+	},
+};
