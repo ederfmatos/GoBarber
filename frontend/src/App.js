ca4a43226510e9dab35985779b4354c0a55f8729
ederfmatos <ederfmatos@gmail.com>
2020-04-08 22:42:42 -0300

    Persistindo autenticação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -7,15 +8,17 @@ import Routes from './routes';
 import GlobalStyle from './styles/globals';
 import history from './services/history';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
