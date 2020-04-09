f6b645a1d2ddfe63ff4512ce1bc00c4431ff8a31
ederfmatos <ederfmatos@gmail.com>
2020-04-08 23:02:27 -0300

    Exibindo toasts na autenticação

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import api from '~/services/api';
 import history from '~/services/history';
@@ -14,16 +15,19 @@ export function* signIn({ payload }) {
       password,
     });
 
+    console.log(response);
+
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('O usuário não é um prestador de serviço');
+      toast.error('O usuário não é um prestador de serviço');
       return;
     }
 
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
