3b08a985f096d201b6b59e7d0e487f9ebc693199
ederfmatos <ederfmatos@gmail.com>
2020-04-08 22:54:23 -0300

    Loading na autenticação

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.svg';
 import { signInRequest } from '~/store/modules/auth/actions';
@@ -14,6 +14,7 @@ const schema = Yup.object().shape({
 
 export default function SignIn() {
   const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
 
   function handleSubmit({ email, password }) {
     dispatch(signInRequest(email, password));
@@ -27,7 +28,9 @@ export default function SignIn() {
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
-        <button type="submit">Acessar</button>
+        <button type="submit" disabled={loading}>
+          {loading ? 'Carregando...' : 'Acessar'}
+        </button>
 
         <Link to="/register">Criar conta</Link>
       </Form>
--- frontend/src/store/modules/auth/actions.js
+++ frontend/src/store/modules/auth/actions.js
@@ -14,6 +14,6 @@ export function signInSuccess(token, user) {
 
 export function signFailure() {
   return {
-    type: '@auth/SIGN_failure',
+    type: '@auth/SIGN_FAILURE',
   };
 }
