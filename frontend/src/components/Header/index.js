c20bb05c41d47fbd95d8181b14886c013601322b
ederfmatos <ederfmatos@gmail.com>
2020-04-09 23:21:41 -0300

    Estilizando Notificações

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import { Container, Content, Profile } from './styles';
 import logo from '~/assets/logo-purple.svg';
 
+import Notifications from '../Notifications';
+
 export default function Header() {
   return (
     <Container>
@@ -14,6 +16,8 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
+
           <Profile>
             <div>
               <strong>Eder Matos</strong>
