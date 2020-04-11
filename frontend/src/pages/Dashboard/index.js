808521adcd6a3c897530d4d39d610f3152df981d
ederfmatos <ederfmatos@gmail.com>
2020-04-10 22:24:44 -0300

    Navegando entre datas

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import api from '~/services/api';
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date],
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
 
-        <strong>31 de maio</strong>
+        <strong>{dateFormatted}</strong>
 
-        <button type="button">
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
