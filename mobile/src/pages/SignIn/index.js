68c2cbd449c8ebe5cd6781f3450b87674de59884
ederfmatos <ederfmatos@gmail.com>
2020-04-17 11:10:12 -0300

    MOBILE - Configurando Background

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
+
+import { Background } from '~/components';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  return <Background />;
 }
