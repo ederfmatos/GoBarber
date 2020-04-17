321f1cf7158751105d4d4fb165e420f1efb2203c
ederfmatos <ederfmatos@gmail.com>
2020-04-17 20:06:36 -0300

    MOBILE - Rota inicial

---
+++ mobile/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import createRoutes from './routes';
+
+export default function App() {
+  const signed = useSelector(state => state.auth.signed);
+
+  const Routes = createRoutes(signed);
+
+  // if (signed) {
+  //   Routes = createUnsignedRoutes();
+  // } else {
+  //   Routes = createSignedRoutes();
+  // }
+
+  return <Routes />;
+}
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,11 +1,59 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { SignIn, SignUp } from './pages';
+// import { SignIn, SignUp, Dashboard } from './pages';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  }),
-);
+// const Tab = createBottomTabNavigator();
+// const Stack = createStackNavigator();
+
+// function getSignedRoutes() {
+//   return (
+//     <Tab.Navigator initialRouteName="Dashboard">
+//       <Tab.Screen name="Dashboard" component={Dashboard} />
+//     </Tab.Navigator>
+//   );
+// }
+
+// function getUnsignedRoutes() {
+//   return (
+//     <Stack.Navigator initialRouteName="SignIn">
+//       <Stack.Screen name="SignIn" component={SignIn} />
+//       <Stack.Screen name="SignUp" component={SignUp} />
+//     </Stack.Navigator>
+//   );
+// }
+
+// export default (isSigned = false) => {
+//   return (
+//     <NavigationContainer>
+//       {isSigned ? getSignedRoutes() : getUnsignedRoutes()}
+//     </NavigationContainer>
+//   );
+// };
+import {
+  createAppContainer,
+  createSwitchNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation';
+
+import { SignIn, SignUp, Dashboard } from './pages';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      },
+    ),
+  );
