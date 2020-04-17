e46c8a10941eebe9aef1f4ca41cf76deebf54b86
ederfmatos <ederfmatos@gmail.com>
2020-04-17 13:52:04 -0300

    MOBILE - Estilizando Botao e Input

--- mobile/src/components/Background/index.js
+++ mobile/src/components/Background/index.js
@@ -1,7 +1,7 @@
-import LineaderGradient from 'react-native-linear-gradient';
+import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
-export default styled(LineaderGradient).attrs({
+export default styled(LinearGradient).attrs({
   colors: ['#7159c1', '#ab59c1'],
 })`
   flex: 1;
--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-import { Background } from '~/components';
+import { Background, Input, Button } from '~/components';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Background />;
+  return (
+    <Background>
+      <Input
+        icon="call"
+        style={{ marginTop: 30 }}
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Clica</Button>
+    </Background>
+  );
 }
