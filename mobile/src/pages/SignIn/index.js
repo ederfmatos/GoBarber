e2bd3ce28f50cfb9723966c96d670783f6124dee
ederfmatos <ederfmatos@gmail.com>
2020-04-17 14:09:11 -0300

    MOBILE - Página SignIn

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import { Image } from 'react-native';
 
-import { Background, Input, Button } from '~/components';
+import logo from '~/assets/logo.png';
+import { Background } from '~/components';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Input
-        icon="call"
-        style={{ marginTop: 30 }}
-        placeholder="Digite seu nome"
-      />
+      <Container>
+        <Image source={logo} />
 
-      <Button>Clica</Button>
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            placeholder="Digite sua senha"
+            secureTextEntry
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink>
+          <SignLinkText>Criar conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
