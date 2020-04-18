604e16396c2ff3d665328b3df0cba3cecf49c8cb
ederfmatos <ederfmatos@gmail.com>
2020-04-18 00:03:40 -0300

    MOBILE - Selecionando horário

--- mobile/src/pages/New/SelectDateTime/index.js
+++ mobile/src/pages/New/SelectDateTime/index.js
@@ -1,15 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { Background, BackTitleButton, DateInput } from '~/components';
 
-import { Container } from './styles';
+import { Container, HourList, Hour, Title } from './styles';
+import api from '~/services/api';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = useMemo(() => navigation.getParam('provider'), []);
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
+
+  const handleSelectTime = useCallback(
+    time =>
+      navigation.navigate('Confirm', {
+        provider,
+        time,
+      }),
+    [],
+  );
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+
+        <HourList
+          data={hours}
+          keyExtractor={({ time }) => time}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => handleSelectTime(item.value)}
+              enabled={item.available}
+            >
+              <Title>{item.time}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   );
